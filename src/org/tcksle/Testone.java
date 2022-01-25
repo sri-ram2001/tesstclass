2c95fe022724a1591cb1d41643b644804891ea57
sri-ram2001 <98308544+sri-ram2001@users.noreply.github.com>
2022-01-25 14:06:16 +0530

    Update Testone.java

--- src/org/tcksle/Testone.java
+++ src/org/tcksle/Testone.java
@@ -3,7 +3,7 @@ package org.tcksle;
 public class Testone {
 public static void main(String[] args) {
 	int a=9;
-    int b=6;
+        int b=6;
 	int c=a+b;
 	System.out.println("hi");
 	System.out.println("mathes arthimatic \n9+6="+c );
