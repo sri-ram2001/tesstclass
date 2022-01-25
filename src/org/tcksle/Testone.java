a2b8ce6d67bde72906f671edff698ff16f2c0e98
sri-ram2001 <srirammnm007@gmail.com>
2022-01-25 14:02:20 +0530

    added

---
+++ src/org/tcksle/Testone.java
@@ -0,0 +1,13 @@
+package org.tcksle;
+
+public class Testone {
+public static void main(String[] args) {
+	int a=9;
+    int b=6;
+	int c=a+b;
+	System.out.println("hi");
+	System.out.println("mathes arthimatic \n9+6="+c );
+	System.out.println("for git");
+	System.out.println("upload");
+}
+}
